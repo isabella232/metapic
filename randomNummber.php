096dc111722b52575e69473d3d27596444220d8c
Erik Axelsson <erikaxelsson1@gmail.com>
2015-03-26 09:01:10 +0100

    test if fix warning for edvin

--- randomNummber.php
+++ randomNummber.php
@@ -5,6 +5,6 @@ $url=$options['uri_string']."/generateIframeRandomCode";
 $response=wp_remote_post("http://".$url,array('body'=>array("email"=>$options['email_string'],"password"=>$options['password_string'])));
 
 $responseObj=json_decode( wp_remote_retrieve_body($response));
-$responseObj->metapicApi =$url=$options['uri_string'];
+$responseObj->metapicApi =$options['uri_string'];
 
 echo json_encode($responseObj);
