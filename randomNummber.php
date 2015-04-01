510a3eccf60eafdb0a59fd40c8386b2b7ce28425
Erik Axelsson <erikaxelsson1@gmail.com>
2015-04-01 17:08:30 +0200

    fix collage can get image

--- randomNummber.php
+++ randomNummber.php
@@ -5,6 +5,8 @@ $url=$options['uri_string']."/generateIframeRandomCode";
 $response=wp_remote_post("http://".$url,array('body'=>array("email"=>$options['email_string'],"password"=>$options['password_string'])));
 
 $responseObj=json_decode( wp_remote_retrieve_body($response));
+
+
 $responseObj->metapicApi =$options['uri_string'];
 
 echo json_encode($responseObj);
