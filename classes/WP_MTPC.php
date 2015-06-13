58841df8836fb2957c20168d48f35029f68ed7b9
Marcus Dalgren <marcus@raket.nu>
2015-06-13 18:34:53 +0200

    Single site first version DONE

--- classes/WP_MTPC.php
+++ classes/WP_MTPC.php
@@ -7,15 +7,36 @@ class WP_MTPC extends stdClass {
 	/* @var ApiClient $client */
 	private $client;
 	private $templateVars = [];
+	private $debugMode = false;
 
 	public function __construct($plugin_dir) {
+		$options = get_option('metapic_options');
+		$this->debugMode = (defined("MTPC_DEBUG") && MTPC_DEBUG === true);
 		$this->plugin_dir = $plugin_dir;
 		$this->plugin_url = plugins_url() . '/' . basename(__DIR__);
-		$this->client = new ApiClient("http://api.metapic.dev", get_site_option("mtpc_api_key"), get_site_option("mtpc_secret_key"));
+		$apiUrl = ($this->debugMode && isset($options["uri_string"])) ? $options["uri_string"] : "http://metapic-testapi.herokuapp.com/";
+		$this->client = new ApiClient($apiUrl, get_site_option("mtpc_api_key"), get_site_option("mtpc_secret_key"));
 		$this->setupOptionsPage();
 		$this->setupLang();
 		$this->setupNetworkOptions();
 		$this->setupIframeRoutes();
+	}
+
+	/**
+	 * Sets a status message for the current option page
+	 * @param string $message   - The message to send
+	 * @param string $class     - Class for the element. Valid options are updated, error or update-nag
+	 */
+	private function setStatusMessage($message, $class = "updated") {
+		add_settings_error('general', 'settings_updated', __($message, "metapic"), $class);
+		/*
+		$statusMessage = function() use ($message, $class) {
+			$message = __($message, "metapic");
+			echo "<div class=\"$class notice is-dismissible\"><p><strong>$message</strong></p></div>";
+		};
+		add_action( 'admin_notices', $statusMessage, 3 );
+		add_action( 'network_admin_notices', $statusMessage, 3 );
+		*/
 	}
 
 	private function setupOptionsPage() {
@@ -35,37 +56,62 @@ class WP_MTPC extends stdClass {
 					default:
 						$options['email_string'] = trim($input['email_string']);
 						$options['password_string'] = trim($input['password_string']);
-						$options['uri_string'] = trim($input['uri_string']);
-						$user = $this->client->login($options['email_string'], $options['password_string']);
+						$options['uri_string'] = trim($input['uri_string'], "/");
+						try {
+							$user = $this->client->login($options['email_string'], $options['password_string']);
+							if ($user) {
+								update_option("mtpc_active_account", true);
+								update_option("mtpc_access_token", $user["access_token"]["access_token"]);
+								$this->setStatusMessage("Login successful");
+							}
+							else {
+								throw new Exception;
+							}
+						}
+						catch (Exception $e) {
+							delete_option("mtpc_active_account");
+							delete_option("mtpc_access_token");
+							$this->setStatusMessage("Invalid username or password", "error");
+						}
 						break;
 				}
 				return $options;
 			});
 
-			add_settings_section('plugin_main', 'Login', function () {
-				echo '<p>Please login to your Metapic account</p>';
-			}, 'plugin');
+			$activeAccount = get_option("mtpc_active_account");
 
-			add_settings_field('email_field', 'Email', function () use ($options) {
-				echo "<input id='plugin_text_string' name='metapic_options[email_string]' size='40' type='text' value='{$options['email_string']}' />";
-			}, 'plugin', 'plugin_main');
+			if (!$activeAccount) {
+				add_settings_section('plugin_main', 'Login', function () {
+					echo '<p>Please login to your Metapic account</p>';
+				}, 'plugin');
+				add_settings_field('email_field', 'Email', function () use ($options) {
+					echo "<input id='plugin_text_string' name='metapic_options[email_string]' size='40' type='text' value='{$options['email_string']}' />";
+				}, 'plugin', 'plugin_main');
 
-			add_settings_field('password_field', 'Password', function () use ($options) {
-				echo "<input id='plugin_text_string' type='password' name='metapic_options[password_string]' size='40' type='text' value='{$options['password_string']}' />";
-			}, 'plugin', 'plugin_main');
+				add_settings_field('password_field', 'Password', function () use ($options) {
+					echo "<input id='plugin_text_string' type='password' name='metapic_options[password_string]' size='40' type='text' value='{$options['password_string']}' />";
+				}, 'plugin', 'plugin_main');
+			}
+			else {
+				add_settings_section('plugin_main', 'Your account', function () {
+					echo '<p>You are currently logged in</p>';
+				}, 'plugin');
+			}
 
-			add_settings_section('plugin_advanced', 'Advanced', function () {
-				echo '<p>Advanced settings</p>';
-			}, 'plugin');
+			if ($this->debugMode) {
+				add_settings_section('plugin_advanced', 'Advanced', function () {
+					echo '<p>Advanced settings</p>';
+				}, 'plugin');
 
-			add_settings_field('uri_field', 'Address to the server', function () use ($options) {
-				echo "<input id='plugin_text_string' name='metapic_options[uri_string]' size='40' type='text' value='{$options['uri_string']}' />";
-			}, 'plugin', 'plugin_advanced');
+				add_settings_field('uri_field', 'Address to the server', function () use ($options) {
+					echo "<input id='plugin_text_string' name='metapic_options[uri_string]' size='40' type='text' value='{$options['uri_string']}' />";
+				}, 'plugin', 'plugin_advanced');
+			}
 		});
 
 		add_action('admin_menu', function () {
 			$isValidClient = get_site_option("mtpc_valid_client");
-			if ($isValidClient) {
+			if (!is_multisite() || (is_multisite() && $isValidClient)) {
 				add_options_page('Metapic', 'Metapic', 'manage_options', 'metapic_settings', function () {
 					if (is_multisite())
 						$this->getTemplate("metapic-options-ms");
--- templates/metapic-options.php
+++ templates/metapic-options.php
@@ -1,8 +1,12 @@
+<?php
+$activeAccount = get_option("mtpc_active_account");
+?>
 <div class="wrap">
 	<h2>Metapic settings page</h2>
 	<form action="options.php" method="post">
 		<?php settings_fields('metapic_options'); ?>
 		<?php do_settings_sections('plugin'); ?>
+		<input type="hidden" name="mtpc_action" value="login"/>
 
 		<p class="submit"><input type="submit" value="<?php esc_attr_e('Save Changes'); ?>" class="button button-primary" id="submit" name="submit"></p>
 	</form>
