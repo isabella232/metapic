552e40f679a004b71728c9f382873998023b6ca4
Marcus Dalgren <marcus@raket.nu>
2015-09-23 15:12:55 +0200

    Use new frontend script

--- templates/frontend-js.php
+++ templates/frontend-js.php
@@ -1,6 +1,2 @@
-
-<script>
-window.$_metapic_user_config = jQuery.extend({"background_off":false,"picture_menu_visible":false,"show_all_tags":false,"strict_mode":true,"can_login":false}, {"show_all_tags":  false});
-</script>
 <link rel="stylesheet" href="//s3-eu-west-1.amazonaws.com/metapic-cdn/site/css/remote/metapic.min.css">
-<script src="//s3-eu-west-1.amazonaws.com/metapic-cdn/site/javascript/metapic/metapic.nattstad.js" id="metapic_load"></script>
+<script src="//s3-eu-west-1.amazonaws.com/metapic-cdn/dev/metapic.preLoginNoLogin.min.js" data-metapic-user-id="<?= get_option("mtpc_id") ?>" id="metapic-load"></script>
