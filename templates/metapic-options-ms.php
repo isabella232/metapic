0e55d3333a818b1d587ef70814f20ddbeef9f739
Marcus Dalgren <marcus@raket.nu>
2015-10-02 10:42:21 +0200

    Move help button to template file, final release adjustments

--- templates/metapic-options-ms.php
+++ templates/metapic-options-ms.php
@@ -6,10 +6,9 @@ $this->blogInfo = get_bloginfo("admin_email");
 $this->mtpcEmail = get_option("mtpc_email");
 /* @ WP_User $this->wp_user */
 $this->wp_user = wp_get_current_user();
-$this->user = $this->client->getUserByEmail($this->wp_user->user_email);
-$this->validUser = isset($user["id"]);
+$this->user = $this->client->getUserByEmail($this->mtpcEmail);
+$this->validUser = isset($this->user["id"]);
 $this->submitText = ($this->hasAccount && $this->user) ? __("Deactivate account", "metapic") : __("Create account", "metapic");
-
 ?>
 <div class="wrap">
 		<?php if (!$this->hasAccount && $this->user) {
