4d197bcc01d46702431d981586fe9b9dfce7d373
Marcus Dalgren <marcus@raket.nu>
2015-10-04 15:40:56 +0200

    Revert to inline

--- templates/help-button.php
+++ templates/help-button.php
@@ -1,1 +1,17 @@
 <a href="#" id="metapic-help-button" class="button"><?= __("How to earn money", "metapic") ?></a>
+<script type="text/javascript">
+	(function($) {
+		$("#metapic-help-button").on("click", function(e) {
+			e.preventDefault();
+			$.getJSON("<?= $this->iframeUrl ?>", function (data) {
+				$.event.trigger({
+					type: "metapic",
+					baseUrl: "<?= $this->getApiUrl() ?>",
+					startPage: "guide",
+					hideSidebar: true,
+					randomKey: data['access_token']['access_token']
+				});
+			});
+		});
+	})(jQuery);
+</script>
